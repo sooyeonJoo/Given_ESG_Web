afd9453755834ac8c0659824126906c2a1d8b60b
HongJiu-wb <jiuwb3@gmail.com>
2024-06-10 14:27:57 +0900

    [Fix] 장고 설정

---
+++ givenWeb/given/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.given_home, name="given_home"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- givenWeb/given/views.py
+++ givenWeb/given/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def given_home(request):
+  return render(request, 'home.html')
