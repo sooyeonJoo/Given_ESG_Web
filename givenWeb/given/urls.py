db77dfe49cce9e11c0007fef5955751f3fee19ea
sooyeonJoo <netwizjoo@gmail.com>
2024-06-10 15:18:47 +0900

    [Fix] template 및 이미지 사이트 경로 수정

--- givenWeb/given/urls.py
+++ givenWeb/given/urls.py
@@ -6,6 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.given_home, name="given_home"),
+    path('heart/', views.given_heart, name="given_heart"),
+    path('money/', views.given_money, name="given_money"),
+    path('mypage/', views.given_mypage, name="given_mypage"),
 ]
 
 if settings.DEBUG:
